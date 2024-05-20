235dc579ea31f1cd066abcc1aa5091b16782a346
Sanjeev <123805942+sanjeev929@users.noreply.github.com>
2024-05-20 14:53:38 +0530

    reg

--- microservice_users/userapp/views.py
+++ microservice_users/userapp/views.py
@@ -4,6 +4,11 @@ def index(request):
     return render(request, 'index.html')
 
 def registration(request):
+    if request.method == 'POST':
+        name = request.POST['username']
+        mail = request.POST['mail']
+        password = request.POST['password1']
+        print(name,mail,password)
     return render(request, 'registration.html')
 
 def login(request):
